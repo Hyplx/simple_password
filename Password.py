598192cd60e89d40c1814180af8843c7cbe34d04
Hyplx <79034075+Hyplx@users.noreply.github.com>
2021-08-10 14:35:42 -0400

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,15 @@
+from getpass import getpass
+import time
+
+username = input("User: ")
+password = input("Pass: ")
+
+time.sleep(.25)
+
+
+if username == 'Name' and password == 'Pass':
+    print('Welcome' ,username)
+    
+
+else:
+    print('Login failed')
